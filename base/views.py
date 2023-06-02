258360f8a6912850883f40b089fc1192d8cef46f
Sanchit <sanchit.n71@gmail.com>
2023-06-03 00:00:04 +0530

    Adding the main files

---
+++ base/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from agora_token_builder import RtcTokenBuilder
+from django.http import JsonResponse
+import random
+import time
+import json
+from .models import RoomMember
+
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+
+def getToken(request):
+    appId = '7c2433b06d734b56a7ab0329d0742843'
+    appCertificate = '47d6bd01ea6c4694bb32b30e2e53ba90'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 250)
+    expirationTimeInSeconds = 3600*24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(
+        appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
+def lobby(request):
+    return render(request, 'lobby.html')
+
+
+def room(request):
+    return render(request, 'room.html')
+
+
+@csrf_exempt
+def createUser(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+
+    return JsonResponse({'name': data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(uid=uid, room_name=room_name,)
+
+    name = member.name
+    return JsonResponse({'name': member.name}, safe=False)
+
+
+@csrf_exempt
+def deleteUser(request):
+    data = json.loads(request.body)
+
+    member = RoomMember.objects.get(name=data['name'], uid=data['UID'], room_name=data['room_name'])
+
+    member.delete()
+
+    return JsonResponse('Member deleted', safe=False)
---
+++ static/js/script.js
@@ -0,0 +1,158 @@
+const APP_ID = "7c2433b06d734b56a7ab0329d0742843"
+const CHANNEL = sessionStorage.getItem('room')
+const TOKEN = sessionStorage.getItem('token')
+let UID = Number(sessionStorage.getItem('UID'))
+let NAME = sessionStorage.getItem('name')
+
+const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
+
+let localTracks = []
+let remoteUsers = {}
+
+// FOR DISPLAYING THE USER
+let joinAndDisplayLocalStream = async () => {
+
+  document.getElementById('room-name').innerText = CHANNEL
+  client.on('user-published', handleUserJoined)
+  client.on('user-left', handleUserLeft)
+
+  try{
+    await client.join(APP_ID, CHANNEL, TOKEN, UID)
+
+  }catch(error){
+    console.error(error)
+    window.open('/','_self')
+  }
+  
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+
+  let member = await createMember()
+
+  let player = `<div class="video-container" id="user-container-${UID}">
+                <div class="username-wrapper"><span id="user-name">${member.name}</span></div>
+                <div class="video-player" id="user-${UID}"></div>
+                </div>`
+
+  document.getElementById("video-streams").insertAdjacentHTML('beforeend', player)
+
+  localTracks[1].play(`user-${UID}`)
+  // localTracks[0].play(`user-${UID}`)
+
+  await client.publish([localTracks[0], localTracks[1]])
+}
+
+// FOR MULTIPLE USER
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user
+  await client.subscribe(user, mediaType)
+
+  if (mediaType === 'video') {
+    let player = document.getElementById(`user-container-${user.uid}`)
+
+    if (player != null) {
+      player.remove()
+    }
+
+    let member = await getMember(user)
+
+    player = `<div class="video-container" id="user-container-${user.uid}">
+              <div class="username-wrapper"><span id="user-name">${member.name}</span></div>
+              <div class="video-player" id="user-${user.uid}"></div>
+              </div>`
+
+    document.getElementById("video-streams").insertAdjacentHTML('beforeend', player)
+
+    user.videoTrack.play(`user-${user.uid}`)
+  }
+
+  if (mediaType === 'audio') {
+    user.audioTrack.play()
+  }
+}
+
+// USER HAS LEFT
+let handleUserLeft = async(user) =>{
+  delete remoteUsers[user.uid]
+  document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+// LEAVE BUTTON
+let leaveAndRemoveStream = async()=>{
+  for (let i = 0; i < localTracks.length; i++) {
+    localTracks[i].stop()
+    localTracks[i].close()
+  }
+
+  await client.leave()
+
+  deleteMember()
+
+  window.open('/','_self')
+}
+
+// FOR VIDEO
+let toggleCamera = async(e) =>{
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false)
+    e.target.style.backgroundColor = "#fff"
+  }
+  else{
+    await localTracks[1].setMuted(true)
+    e.target.style.backgroundColor = "rgb(196, 44, 44)"
+  }
+}
+
+let toggleMic = async(e) =>{
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false)
+    e.target.style.backgroundColor = "#fff"
+  }
+  else{
+    await localTracks[0].setMuted(true)
+    e.target.style.backgroundColor = "rgb(196, 44, 44)"
+  }
+}
+
+let createMember = async()=>{
+  let response = await fetch('/create_member/',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({'name':NAME,'room_name':CHANNEL,'UID':UID})
+  })
+
+  let member = await response.json()
+  return member
+}
+
+
+let getMember = async(user) =>{
+  let response = await fetch(`/get_member/?UID=${user.uid}&room_name=${CHANNEL}`)
+  let member = await response.json()
+  return member
+}
+
+
+let deleteMember = async()=>{
+  let response = await fetch('/delete_member/',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({'name':NAME,'room_name':CHANNEL,'UID':UID})
+  })
+
+  let member = await response.json()
+}
+
+
+joinAndDisplayLocalStream()
+
+window.addEventListener('beforeunload', deleteMember)
+
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveStream)
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
